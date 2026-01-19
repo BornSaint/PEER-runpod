f9ed3e00279bd5d286433154471799366800cc3e
Róger Santos <34362175+BornSaint@users.noreply.github.com>
2026-01-19 00:11:39 -0300

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,39 +1,22 @@
-# # Install common libraries
-# install hf-cli
-curl -LsSf https://hf.co/cli/install.sh | bash
-# pip install -r requirements.txt
-# install uv python
-# wget -qO- https://astral.sh/uv/install.sh | sh
-curl -LsSf https://astral.sh/uv/install.sh | sh
-# uv init .
 
-source $HOME/.local/bin/env
-uv venv --python 3.12 --project .
-# source .venv/bin/activate
-uv sync --python 3.12 --project .
-# uv add -r requirements.txt --python 3.12
-# ls -a 
-# ls -a 
-# sleep infinity
+apt install python3.12 python3.12-dev python3.12-venv
+cd /PEER-runpod
+source ./.venv/bin/activate
+
 # Check if HUGGINGFACE_TOKEN is set and log in to Hugging Face
 if [ -n "$HUGGINGFACE_TOKEN" ]; then
     echo "HUGGINGFACE_TOKEN is defined. Logging in..."
     # hf auth login --token $HUGGINGFACE_TOKEN --add-to-git-credential
-    uvx hf auth login --token $HUGGINGFACE_TOKEN --add-to-git-credential
+    hf auth login --token $HUGGINGFACE_TOKEN --add-to-git-credential
 fi
 
 if [ "$DEBUG" == "True" ]; then
     echo "Launch Finetune in debug mode"
 fi
 
-
-# python "./main.py"
-# .venv/bin/torchrun --nproc_per_node=1 main.py
-# source .venv/bin/activate
-/PEER-runpod/.venv/bin/torchrun --nproc_per_node=1 main.py 
-
+python3.12 -m torchrun --nproc_per_node=1 main.py
 # hf upload BornSaint/PEER-weights final_peer_language_model.pth --private
-uvx hf upload BornSaint/PEER-weights /PEER-runpod/final_peer_language_model.pth --private
+hf upload BornSaint/PEER-weights /PEER-runpod/final_peer_language_model.pth --private
 
 if [ "$DEBUG" == "False" ]; then
     runpodctl remove pod $RUNPOD_POD_ID
